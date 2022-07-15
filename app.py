88918a39765a28626647a957d90e3f2c2372ba12
Peter Yeh <yehyuchun2@gmail.com>
2022-07-15 23:12:53 +0800

    Fix python path escapting error

--- app.py
+++ app.py
@@ -1,29 +1,22 @@
 import os
 import sqlite3
-# from os.path import dirname, join, realpath
 
 import pandas as pd
 from flask import Flask, redirect, render_template, request, url_for
 
+# from os.path import dirname, join, realpath
+
+
 app = Flask(__name__)
 
-# enable debugging mode
 app.config["DEBUG"] = True
+SAVED_DIR = os.path.join(os.getcwd(), 'static', 'files')
 
 
 def get_db_connection():
     conn = sqlite3.connect('database.db')
     conn.row_factory = sqlite3.Row
     return conn
-
-
-# Upload folder
-UPLOAD_FOLDER = 'static/files'
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
-
-cwd = os.getcwd()
-SAVED_DIR = os.path.join(cwd, 'static/files')
-
 
 
 # Root URL
@@ -39,11 +32,8 @@ def upload_files():
     # get the uploaded file
     uploaded_file = request.files['file']
     if uploaded_file.filename != '':
-        file_path = os.path.join(app.config['UPLOAD_FOLDER'], uploaded_file.filename)
-       # set the file path
+        file_path = os.path.join(SAVED_DIR, uploaded_file.filename)
         uploaded_file.save(file_path)
-
-        parse_CSV('static\files\Input1.xlsx')
         parse_CSV(file_path)
 
     return redirect(url_for('index'))
@@ -52,7 +42,7 @@ def upload_files():
 def parse_CSV(filePath):
     col_names = ['id', 'login', 'name', 'salary']
     csvData = pd.read_csv(filePath, names=col_names, header=None)
-    # Loop through the Rows
+
     for i, row in csvData.iterrows():
         print(i, row['id'], row['login'], row['name'], row['salary'])
 
